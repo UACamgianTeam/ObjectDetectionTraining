31d6c68b94bcc3f8530511e3269f34060bc3d60b
stephenhgregory <shgregory@icloud.com>
2020-08-27 15:53:12 -0500

    Continued to complete retrain_model.sh to take user input which will choose which model to retrain

--- models/retrain_model.sh
+++ models/retrain_model.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 # Used to train TensorFlow 1 models
 
+# Save the current repo name
+OBJECTDETECTIONTRAINING_REPO=$(pwd)
+
 # Find the tensorflow/models/research repo
 TENSORFLOW_MODEL_REPO=$(find ~ -type d -wholename "*/tensorflow/models/research" )
 
@@ -8,19 +11,40 @@ TENSORFLOW_MODEL_REPO=$(find ~ -type d -wholename "*/tensorflow/models/research"
 { cd "$TENSORFLOW_MODEL_REPO" ; echo "tensorflow/models repo found at $TENSORFLOW_MODEL_REPO"; } || \
 { echo "Could not find tensorflow/models/research repository. Clone that repo and try again!" ; exit 1; }
 
-# Set the path to the pipeline.config file
-PIPELINE_CONFIG_PATH={path to pipeline config file}
-# Set the path to the model directory to save
-MODEL_DIR={path to model directory}
+# Get the model type that the user wishes to retrain
+printf("Which model do you want to retrain?\n")
+printf("1. ssd_mobilenet_v2_coco_2019_03_29\n")
+printf("Select a number: ")
+read MODEL_NUM
+
+# Set the path to the pipeline.config file and the output model directory based upon the model type
+if [ $MODEL_NUM == 1 ] # ssd_mobilenet_v2_coco_2018_03_29
+then
+  PIPELINE_CONFIG_PATH={"${OBJECTDETECTIONTRAINING_REPO}/original_models/ssd_mobilenet_v2_coco_2018_03_29/pipeline.config"}
+  MODEL_DIR={"${OBJECTDETECTIONTRAINING_REPO}/retrained_models/ssd_mobilenet_v2_coco_2018_03_29"}
+else
+  { echo "Try again. \n" ; exit 1; }
+fi
+
 # Set the number of training steps
 NUM_TRAIN_STEPS=50000
 # Sample 1 out of every N evaluation examples for eval
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 
-# Run the training script
-python object_detection/legacy/train.py \
-    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-    --model_dir=${MODEL_DIR} \
-    --num_train_steps=${NUM_TRAIN_STEPS} \
-    --sample_1_of_n_eval_examples=${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
-    --alsologtostderr
+# (For debugging) print out our variables
+echo "PIPELINE_CONFIG_PATH: ${PIPELINE_CONFIG_PATH}"
+echo "MODEL_DIR: ${MODEL_DIR}"
+echo "NUM_TRAIN_STEPS: ${NUM_TRAIN_STEPS}"
+echo "SAMPLE_1_OF_N_EVAL_EXAMPLES: ${SAMPLE_1_OF_N_EVAL_EXAMPLES}"
+
+exit 0
+
+
+## Run the training script
+#echo "Running retraining script..."
+#python object_detection/legacy/train.py \
+#    --pipeline_config_path="${PIPELINE_CONFIG_PATH}" \
+#    --model_dir="${MODEL_DIR}" \
+#    --num_train_steps=${NUM_TRAIN_STEPS} \
+#    --sample_1_of_n_eval_examples=${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
+#    --alsologtostderr
