16c6ae4e291ecb0dc08af9b5f885b1ee3d2f903c
stephenhgregory <shgregory@icloud.com>
2020-09-02 19:28:39 -0500

    Fixing label map to remove ID #10 (Traffic Light), and augmenting retrain_model.sh and evaluate_model.sh

--- models/evaluate_model.sh
+++ models/evaluate_model.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Used to train TensorFlow 1 models
+# Used to evaluate models
 
 # Find and store the current path, which should be .../ObjectionDetectionTraining/models
 OBJECTDETECTIONTRAINING_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -15,8 +15,8 @@ else
   cd "$TENSORFLOW_MODEL_REPO" && echo "tensorflow/models repo found at $TENSORFLOW_MODEL_REPO"
 fi
 
-# Get the model type that the user wishes to retrain
-printf "Which model do you want to retrain?\n"
+# Get the model type that the user wishes to evaluate
+printf "Which model do you want to evaluate?\n"
 printf "1. ssd_mobilenet_v2_coco_2019_03_29\n"
 printf "\n"
 printf "Select a number: "
@@ -25,21 +25,23 @@ read -r MODEL_NUM
 # Set the path to the pipeline.config file and the output model directory based upon the model type
 if [ "$MODEL_NUM" == 1 ] # ssd_mobilenet_v2_coco_2018_03_29
 then
-  PIPELINE_CONFIG_PATH="${OBJECTDETECTIONTRAINING_REPO}/original_models/ssd_mobilenet_v2_coco_2018_03_29/pipeline.config"
-  TRAIN_DIR="${OBJECTDETECTIONTRAINING_REPO}/retrained_models/ssd_mobilenet_v2_coco_2018_03_29"
+  PIPELINE_CONFIG_PATH="${OBJECTDETECTIONTRAINING_REPO}/retrained_models/ssd_mobilenet_v2_coco_2018_03_29/pipeline.config"
+  ORIGINAL_MODEL_DIR="${OBJECTDETECTIONTRAINING_REPO}/original_models/ssd_mobilenet_v2_coco_2018_03_29"
+  RETRAINED_MODEL_DIR="${OBJECTDETECTIONTRAINING_REPO}/retrained_models/ssd_mobilenet_v2_coco_2018_03_29"
 else
   { echo "Selection ${MODEL_NUM} not found. Try again, make sure the number you enter matches the model you wish to train!" ; exit 1; }
 fi
 
 # (For debugging) print out our variables
 printf "\nPIPELINE_CONFIG_PATH: %s\n" "$PIPELINE_CONFIG_PATH"
-printf "TRAIN_DIR: %s\n\n" "$TRAIN_DIR"
+printf "RETRAINED_MODEL_DIR: %s\n\n" "$RETRAINED_MODEL_DIR"
+printf "ORIGINAL_MODEL_DIR: %s\n\n" "$ORIGINAL_MODEL_DIR"
 
 # Update PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 
-## Run the training script
-echo "Running retraining script..."
-python object_detection/legacy/train.py \
+# Run the evaluation script
+echo "Running evaluation script..."
+python object_detection/legacy/evaluate.py \
     --pipeline_config_path="${PIPELINE_CONFIG_PATH}" \
     --train_dir="${TRAIN_DIR}"
