b3566df81894bf39dbf8392cfee77681c3925002
stephenhgregory <shgregory@icloud.com>
2020-12-02 12:05:14 -0600

    Added one missing conversion in voc_to_coco_annotations.sh

--- DataManipulation/voc_to_coco_annotations.sh
+++ DataManipulation/voc_to_coco_annotations.sh
@@ -38,6 +38,11 @@ python3 voc_to_coco_annotations.py \
 --output_file ../data/PEViD-UHD/fighting_day_indoor_1/annotations/fighting_day_indoor_1.json
 
 python3 voc_to_coco_annotations.py \
+--ann_paths_list ../data/PEViD-UHD/fighting_day_indoor_3/annotations/annpaths_list.txt \
+--labels ../data/PEViD-UHD/labels.txt \
+--output_file ../data/PEViD-UHD/fighting_day_indoor_3/annotations/fighting_day_indoor_3.json
+
+python3 voc_to_coco_annotations.py \
 --ann_paths_list ../data/PEViD-UHD/fighting_day_indoor_2/annotations/annpaths_list.txt \
 --labels ../data/PEViD-UHD/labels.txt \
 --output_file ../data/PEViD-UHD/fighting_day_indoor_2/annotations/fighting_day_indoor_2.json
